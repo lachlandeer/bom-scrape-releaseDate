bf6cf618db14e390b0480cb9b288f80f81bd3859
lachlan <lachlan.deer@uzh.ch>
2016-10-06 18:19:54 -0700

    files added that get all box office mojo links for years 2010-2016

---
+++ src/getLinksAllYears.py
@@ -0,0 +1,35 @@
+# User must set this
+
+datadir = 'links/'
+
+# Import packages
+
+import os
+import time
+import mojoScrapeLinks as mojo
+
+# check directories
+print("Path at terminal when executing this file")
+print(os.getcwd() + "\n")
+
+if not os.path.exists(datadir):
+    os.makedirs(datadir)
+
+# Candidate Release Types
+releaseType =['wide', 'limited']
+
+# Candidate years
+relevantYears = range(2010,2017)
+
+
+# Run scraper
+for iYear in relevantYears:
+    for iType in releaseType:
+        if iType == 'wide':
+            print('Entering wide release mode')
+            typeString = 'widedate'
+            mojo.scrapeLinks(typeString, iYear, iType, datadir)
+        else:
+            print('Entering limited release mode')
+            typeString = 'limited'
+            mojo.scrapeLinks(typeString, iYear, iType, datadir)
